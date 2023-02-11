349a0228336aab386b5f5ce0e0c638710b29a014
Sean Michael Suntoso <ssuntoso@gmail.com>
2023-02-11 09:49:15 +0800

    analytic bug fix

--- src/index.js
+++ src/index.js
@@ -3,24 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const measurement_id = process.env.REACT_APP_MEASUREMENT_ID
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <script async src={`https://www.googletagmanager.com/gtag/js?id=${measurement_id}`}></script>
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', "${measurement_id}", {
-            page_path: window.location.pathname,
-          });
-        `,
-      }}
-    />
     <App />
   </React.StrictMode>
 );
